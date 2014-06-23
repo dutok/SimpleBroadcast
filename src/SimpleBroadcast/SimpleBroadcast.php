814d4811d47024f898f069734f1591fb0e10c456
Dutok <admin@axim.io>
2014-06-23 18:45:59 -0400

    Plugin skeleton

---
+++ src/SimpleBroadcast/SimpleBroadcast.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: Michael Leahy
+ * Date: 6/23/14
+ * Time: 6:20 PM
+ */
+
+namespace SimpleBroadcast;
+
+use pocketmine\plugin\PluginBase;
+
+class SimpleBroadcast extends PluginBase{
+
+    public function onEnable(){
+
+    }
+
+    public function onDisable(){
+
+    }
+
+} 
