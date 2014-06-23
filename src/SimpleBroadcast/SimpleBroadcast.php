18e83c843c0e067f65b168352ac2ff9ef84740e9
Dutok <admin@axim.io>
2014-06-23 19:01:05 -0400

    Add config file and merge classes

--- src/SimpleBroadcast/SimpleBroadcast.php
+++ src/SimpleBroadcast/SimpleBroadcast.php
@@ -8,15 +8,36 @@
 namespace SimpleBroadcast;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Server;
 
 class SimpleBroadcast extends PluginBase{
 
-    public function onEnable(){
+    public $configFile;
 
+    public function onEnable(){
+        @mkdir($this->getDataFolder());
+        $this->configFile = (new Config($this->getDataFolder()."config.yml", Config::YAML, array(
+            "messages" => array(
+                "message1",
+                "message2",
+                "message3"
+            ),
+            "prefix" => "Broadcast"
+        )))->getAll();
+        $this->getServer()->getScheduler()->scheduleRepeatingTask(new SimpleBroadcastTask($this), 120);
     }
 
     public function onDisable(){
 
     }
 
-} 
+}
+
+class SimpleBroadcastTask extends PluginTask{
+
+    public function onRun($currentTick){
+        //Put code to execute on 'task'
+    }
+}
